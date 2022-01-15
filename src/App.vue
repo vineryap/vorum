34803d6027ac4e570c2aff13e066eaf767903664
Vin E. R. Yap <vineryap@gmail.com>
2022-01-15 12:37:39 +0900

    set app

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,40 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import { ref } from 'vue';
+import { mapActions } from '@/helpers';
+import BaseLayout from './layouts/BaseLayout.vue';
+import NProgress from 'nprogress'
+import { useRouter } from 'vue-router'
+
+const { fetchAuthUser } = mapActions('auth')
+const router = useRouter()
+const showPage = ref(false)
+
+function onPageReady() {
+  showPage.value = true
+  NProgress.done()
+}
+
+fetchAuthUser()
+
+NProgress.configure({ showSpinner: false })
+router.beforeEach(() => {
+  showPage.value = false
+  NProgress.start()
+})
 </script>
 
 <template>
-
+  <base-layout>
+    <router-view
+      v-show="showPage"
+      @pageReady="onPageReady"
+      :key="`${$route.path}${JSON.stringify($route.query)}`"
+    />
+    <base-spinner v-show="!showPage" />
+    <base-notifications />
+  </base-layout>
 </template>
 
 <style>
+@import "assets/style.css";
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,31 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 import 'virtual:windi.css'
+import App from './App.vue'
+import 'nprogress/nprogress.css'
+import router from '@/router'
+import store from '@/store'
+import FontAwesome from '@/plugins/fontawesome'
+import ClickOutsideDirective from '@/plugins/ClickOutsideDirective'
+import PageScrollDirective from '@/plugins/PageScrollDirective'
+import VuePagination from '@/plugins/VuePagination'
+import VeeValidatePlugin from '@/plugins/VeeValidatePlugin'
 
-createApp(App).mount('#app')
+const ForumApp = createApp(App)
+ForumApp.use(router)
+ForumApp.use(store)
+ForumApp.use(FontAwesome)
+ForumApp.use(ClickOutsideDirective)
+ForumApp.use(PageScrollDirective)
+ForumApp.use(VuePagination)
+ForumApp.use(VeeValidatePlugin)
+
+const requireComponents = import.meta.globEager('./components/**/Base*.vue')
+for (const path in requireComponents) {
+  const module = requireComponents[path]
+  const component = module.default || module
+  const componentName =
+    component.name || path.replace(/^.+\//, '').replace(/\.\w+$/, '')
+  ForumApp.component(componentName, component)
+}
+
+ForumApp.mount('#app')
