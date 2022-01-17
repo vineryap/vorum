74f9a32747dd2e1610f020d9d69aa9ace505c273
Vin E. R. Yap <vineryap@gmail.com>
2022-01-17 17:52:21 +0900

    refactor

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,52 +1,36 @@
 <template>
   <div class="flex flex-col flex-wrap sm:flex-row">
-    <category-list v-if="pageLoadStatus_pageReady" :categories="categories" />
+    <category-list v-if="isPageReady" :categories="categories" />
   </div>
-
-  <!-- <div class="container">
-    <div class="col-full">
-      <category-list v-if="pageLoadStatus_pageReady" :categories="categories" />
-    </div>
-  </div>-->
 </template>
 
-<script>
+<script setup>
 import CategoryList from '@/components/CategoryList.vue'
-import { mapActions } from 'vuex'
-import { pageLoadStatus } from '@/mixins'
+import { mapState, mapActions } from '@/helpers'
+import usePageLoadStatus from '@/composables/usePageLoadStatus'
+import { defineEmits } from 'vue'
 
-export default {
-  components: {
-    CategoryList
-  },
-  mixins: [pageLoadStatus],
-  computed: {
-    categories() {
-      return this.$store.state.categories.items
-    }
-  },
-  methods: {
-    ...mapActions({
-      fetchCategories: 'categories/fetchCategories',
-      fetchForumsByIds: 'forums/fetchForumsByIds',
-      fetchThreadsByIds: 'threads/fetchThreadsByIds',
-      fetchUsersByIds: 'users/fetchUsersByIds'
-    })
-  },
-  async created() {
-    const categories = await this.fetchCategories()
-    const forumIds = categories.map((category) => category.forums).flat()
-    const forums = await this.fetchForumsByIds({ ids: forumIds })
-    const lastThreadIds = forums
-      .map((f) => f.threads?.at(-1))
-      .filter((id) => id)
-    const threads = await this.fetchThreadsByIds({ ids: lastThreadIds })
-    const userIds = threads.map((thread) => thread.userId)
-    await this.fetchUsersByIds({ ids: userIds })
-    this.pageLoadStatus_pageLoaded()
-  }
+const emit = defineEmits(['pageReady'])
+const { isPageReady, pageLoaded } = usePageLoadStatus()
+
+const { items: categories } = mapState('categories')
+const { fetchCategories } = mapActions('categories')
+const { fetchForumsByIds } = mapActions('forums')
+const { fetchThreadsByIds } = mapActions('threads')
+const { fetchUsersByIds } = mapActions('users')
+
+const initFetch = async () => {
+  const allCategories = await fetchCategories()
+  const forumIds = allCategories.map((category) => category.forums).flat()
+  const forums = await fetchForumsByIds({ ids: forumIds })
+  const lastThreadIds = forums.map((f) => f.threads?.at(-1)).filter((id) => id)
+  const threads = await fetchThreadsByIds({ ids: lastThreadIds })
+  const userIds = threads.map((thread) => thread.userId)
+  await fetchUsersByIds({ ids: userIds })
+  pageLoaded(emit)
 }
+
+initFetch()
 </script>
 
-<style>
-</style>
+<style></style>
